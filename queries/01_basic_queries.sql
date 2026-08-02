541b65a387fad1b521b6d48266d38de90f90fb83
Adarsh Shukla <113442469+PixelNomad-lang@users.noreply.github.com>
2026-08-03 04:46:56 +0530

    Basic Q2 completed

--- queries/01_basic_queries.sql
+++ queries/01_basic_queries.sql
@@ -1,3 +1,12 @@
 -- Q1 Total number of customers find karo.
 Select Count(*)  from customers;
+-----------------------------------------------
+--Q2 Count no of customer from different city 
+SELECT 
+    city_name,
+    COUNT(*) AS customer_count
+FROM customers
+GROUP BY city_name;
+
+----------------------------------------------------
 
