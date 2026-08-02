bac525b4a55264ba9c34b60f487163ee1800ccb5
Adarsh Shukla <113442469+PixelNomad-lang@users.noreply.github.com>
2026-08-03 03:55:02 +0530

    Inserting

---
+++ schema/02_insert_categories.sql
@@ -0,0 +1,9 @@
+-- 02_insert_categories.sql
+
+INSERT INTO categories (category_id, category_name, description)
+VALUES
+(1, 'Electronics', 'Electronic gadgets and devices'),
+(2, 'Clothing', 'Men and Women Clothing'),
+(3, 'Books', 'Books and Magazines'),
+(4, 'Furniture', 'Home and Office Furniture'),
+(5, 'Sports', 'Sports Equipment');
